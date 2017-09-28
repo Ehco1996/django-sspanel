506d1ac90e9f3d85ae2b632f8adefb5764451c71
ehco1996 <zh19960202@gmail.com>
2017-09-28 14:25:32 +0800

    更新计划任务配置

--- django-sspanel/settings.py
+++ django-sspanel/settings.py
@@ -141,7 +141,7 @@ CRONJOBS = [
      '>>' + BASE_DIR + '/logs/userstate.log'),  # 每天23.59分检测用户等级是否到期，日志写入logs
     ('0 0 1 * *', 'ssserver.views.auto_reset_traffic',
      '>>' + BASE_DIR + '/logs/trafficrest.log'),  # 每月月初重置免费用户流量，日志写入logs
-    ('0 1 1 * *', 'ssserver.views.auto_reset_traffic',
+    ('0 1 1 * *', 'ssserver.views.clean_traffic_log',
      '>>' + BASE_DIR + '/logs/trafficrest.log'),  # 每月第一天凌晨1点删除所有流量记录，日志写入logs
 
 ]
