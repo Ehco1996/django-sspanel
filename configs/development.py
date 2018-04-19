befea375a2c46eaef0f1ad170c66ae018c1a031d
ehco1996 <zh19960202@gmail.com>
2018-04-19 09:19:42 +0800

    fix purchasehistory templates

--- configs/development.py
+++ configs/development.py
@@ -6,9 +6,8 @@ from configs.default import *
 DEBUG = True
 
 DATABASES['default'].update(
-    {'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-     'HOST': os.getenv('MYSQL_HOST')
+    {'PASSWORD': os.getenv('MYSQL_PASSWORD',),
+     'HOST': os.getenv('MYSQL_HOST','127.0.0.1')
      })
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
