bbd7af76f099c464a6a7d2c21f830fdaadf10be7
ehco1996 <zh19960202@gmail.com>
2018-06-22 08:26:38 +0800

    add cmd

---
+++ commands/export_node_host.py
@@ -0,0 +1,16 @@
+from __init__ import *
+
+
+def export_node_host():
+    from apps.ssserver.models import Node
+    hosts = []
+    for node in Node.objects.all():
+        hosts.append(node.server)
+
+    with open('node_host.txt', 'w') as f:
+        f.writelines(hosts)
+    print('export node host down ! node num: ', len(hosts))
+
+
+if __name__ == '__main__':
+    export_node_host()
