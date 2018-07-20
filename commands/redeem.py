4dfdb9cae7fdf0d83eb102241dbf6607ce7cb227
ehco1996 <zh19960202@gmail.com>
2018-07-20 08:04:58 +0800

    add redemm cmd

---
+++ commands/redeem.py
@@ -0,0 +1,14 @@
+def pay_redeem():
+    '''给大于3级的用户每人发10元'''
+    from apps.sspanel.models import User
+    users = User.objects.filter(level__gte=3)
+    print('pay user count is: {}'.format(users.count()))
+    for user in users:
+        user.balance += 10
+        print(user.username, 'balance + 3 down')
+
+
+if __name__ == '__main__':
+    from importlib import import_module
+    import_module('__init__', 'commands')
+    pay_redeem()
