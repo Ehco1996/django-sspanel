ca4d57a04cbaaa555c74adc78bc0d87509e519f6
ehco1996 <zh19960202@gmail.com>
2018-07-24 10:37:35 +0800

    safe cmd

--- commands/redeem.py
+++ commands/redeem.py
@@ -1,6 +1,14 @@
+import sys
+
+
 def pay_redeem():
     '''给大于3级的用户每人发10元'''
     from apps.sspanel.models import User
+    user_check = input('即将给所有大于3级的用户发送10元红包，键入 y键 确认操作\n')
+    if user_check != 'y':
+        print('取消操作')
+        sys.exit()
+
     users = User.objects.filter(level__gte=3)
     print('pay user count is: {}'.format(users.count()))
     for user in users:
