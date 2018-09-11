0114c7667a5cf9424d92e714b84f08c8f8c24853
ehco1996 <zh19960202@gmail.com>
2018-09-11 11:48:28 +0800

    fix typo

--- configs/production.py
+++ configs/production.py
@@ -7,7 +7,7 @@ DEBUG = False
 DATABASES['default'].update(
     {'PASSWORD': os.getenv('MYSQL_PASSWORD', 'your_pass'),
      'HOST': os.getenv('MYSQL_HOST', '127.0.0.1'),
-     'USER': os.getenv('MYSQL_HOST', 'root')
+     'USER': os.getenv('MYSQL_USER', 'root')
      })
 
 
