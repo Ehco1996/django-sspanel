14460272feac99cd89e871a9bd8fb914e6a295e8
Ehco1996 <zh19960202@gmail.com>
2020-08-27 08:01:51 +0800

    add redis

--- apps/ext.py
+++ apps/ext.py
@@ -1,13 +1,19 @@
+from redis import Redis
+
+from django.conf import settings
+
 from apps.extensions.alipay import Pay
-from apps.extensions.cachext import Cache
+from apps.extensions.cachext import RedisCache
 from apps.extensions.encoder import Encoder
 
 # register pay instance
 pay = Pay()
 
+# register redis client
+redis = Redis.from_url(settings.REDIS_URI)
 
 # register cache
-cache = Cache()
+cache = RedisCache(settings.REDIS_URI)
 
 # register encoder
 encoder = Encoder()
---
+++ configs/default/redis.py
@@ -0,0 +1,4 @@
+import os
+
+
+REDIS_URI = os.getenv("REDIS_URI", "redis://127.0.0.1:6379")
