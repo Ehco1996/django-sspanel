69c71f780a1c8ff5a50c07f113a14876bcc06a2f
Gkirito <Gkirito@users.noreply.github.com>
2020-09-03 07:45:52 +0800

    fix can't connect 127.0.0.1:6379(redis) (#386)

--- configs/default/redis.py
+++ configs/default/redis.py
@@ -1,4 +1,5 @@
 import os
 
-REDIS_DB_URI = os.getenv("REDIS_DB_URI", "redis://127.0.0.1:6379/0")
-REDIS_CACHE_URI = os.getenv("REDIS_CACHE_URI", "redis://127.0.0.1:6379/1")
+REDIS_HOST = os.getenv("REDIS_HOST", "127.0.0.1")
+REDIS_DB_URI = os.getenv("REDIS_DB_URI", "redis://"+REDIS_HOST+":6379/0")
+REDIS_CACHE_URI = os.getenv("REDIS_CACHE_URI", "redis://"+REDIS_HOST+":6379/1")
