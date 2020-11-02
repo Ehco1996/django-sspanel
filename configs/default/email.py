a18fc2597d444bffacafcedb97385eccf9562293
Gkirito <lownkid@outlook.com>
2020-11-02 16:32:16 +0800

    fix and update
    修复docker-compose run --rm web python manage.py collectstatic --noinput可能出现mysql启动缓慢，导致web连接数据库失败问题，优化docker-compose配置

--- configs/default/email.py
+++ configs/default/email.py
@@ -4,7 +4,7 @@ import os
 USE_SMTP = bool(os.getenv("USE_SMTP", True))
 EMAIL_USE_SSL = bool(os.getenv("EMAIL_USE_SSL", True))
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.163.com")
-EMAIL_PORT = os.getenv("EMAIL_PORT", 465)
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 465))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
