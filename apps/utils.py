96451d053a0f4fbc624fcac57bfcb3cd34fe10a4
ehco1996 <zh19960202@gmail.com>
2021-05-05 17:04:15 +0800

    feat: add x_forwarded_for in uwsgi log

--- apps/utils.py
+++ apps/utils.py
@@ -95,6 +95,7 @@ def gen_datetime_list(t: pendulum.DateTime, days: int = 6):
 
 def get_client_ip(request):
     x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
+    print(f"x_forwarded_for: {x_forwarded_for}")
     if x_forwarded_for:
         return x_forwarded_for.split(",")[0]
     else:
