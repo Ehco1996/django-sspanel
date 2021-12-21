edb5ee91a1a1ce70177936c7575f0e10bc28993c
ehco1996 <zh19960202@gmail.com>
2021-12-21 17:34:14 +0800

    revert admin

--- apps/proxy/admin.py
+++ apps/proxy/admin.py
@@ -115,6 +115,21 @@ class RelayNodeAdmin(admin.ModelAdmin):
     api_endpoint.action_type = 2
 
 
+class RelayRuleAdmin(admin.ModelAdmin):
+    list_display = [
+        "rule_name",
+        "proxy_node",
+        "relay_node",
+        "relay_host",
+        "relay_port",
+        "remark",
+        "enable",
+    ]
+    search_fields = []
+    list_filter = ["relay_node", "proxy_node"]
+    inlines = []
+
+
 class UserTrafficLogAdmin(admin.ModelAdmin):
     list_display = [
         "username",
@@ -146,5 +161,6 @@ class UserTrafficLogAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(models.ProxyNode, ProxyNodeAdmin)
 admin.site.register(models.RelayNode, RelayNodeAdmin)
+admin.site.register(models.RelayRule, RelayRuleAdmin)
 
 admin.site.register(models.UserTrafficLog, UserTrafficLogAdmin)
