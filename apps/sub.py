9ab561ff00df4bf671087c0a824f4724bf757627
ehco1996 <zh19960202@gmail.com>
2022-08-29 08:39:28 +0800

    sub: user clash providers as normal sub

--- apps/sub.py
+++ apps/sub.py
@@ -8,14 +8,12 @@ from django.template.loader import render_to_string
 class UserSubManager:
     """统一管理用户的订阅"""
 
-    SUB_TYPE_SS = "ss"
-    SUB_TYPE_TROJAN = "trojan"
+    SUB_TYPE_NORMAL = "NORMAL"
     SUB_TYPE_CLASH = "clash"
     SUB_TYPE_CLASH_PRO = "clash_pro"
 
     SUB_TYPES_SET = {
-        SUB_TYPE_SS,
-        SUB_TYPE_TROJAN,
+        SUB_TYPE_NORMAL,
         SUB_TYPE_CLASH,
         SUB_TYPE_CLASH_PRO,
     }
@@ -23,31 +21,14 @@ class UserSubManager:
     def __init__(self, user, sub_type, node_list):
         self.user = user
         if sub_type not in self.SUB_TYPES_SET:
-            sub_type = self.SUB_TYPE_SS
+            sub_type = self.SUB_TYPE_NORMAL
         self.sub_type = sub_type
         self.node_list = node_list
 
     def get_sub_info(self):
         if self.sub_type in [self.SUB_TYPE_CLASH, self.SUB_TYPE_CLASH_PRO]:
             return self.get_clash_sub_yaml()
-        return self.get_ss_sub_links()
-
-    def get_ss_sub_links(self):
-        sub_links = ""
-        relay_node_group = defaultdict(list)
-        for node in self.node_list:
-            if node.enable_relay:
-                for rule in node.relay_rules.filter(relay_node__enable=True):
-                    relay_node_group[rule.relay_node].append(
-                        node.get_user_node_link(self.user, rule)
-                    )
-            if node.enable_direct:
-                sub_links += node.get_user_node_link(self.user) + "\n"
-        for sub_link_list in relay_node_group.values():
-            for link in sub_link_list:
-                sub_links += link + "\n"
-        sub_links = base64.urlsafe_b64encode(sub_links.encode()).decode()
-        return sub_links
+        return self.get_clash_proxy_providers()
 
     def get_clash_sub_yaml(self):
         return render_to_string(
