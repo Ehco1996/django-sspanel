b30939be2b64184df0ed61091ac90b5ab4303d2c
ehco1996 <zh19960202@gmail.com>
2022-08-31 08:27:46 +0800

    sub: revert ss sub link back

--- apps/sub.py
+++ apps/sub.py
@@ -1,3 +1,4 @@
+import base64
 from collections import defaultdict
 
 from django.conf import settings
@@ -7,11 +8,13 @@ from django.template.loader import render_to_string
 class UserSubManager:
     """统一管理用户的订阅"""
 
+    SUB_TYPE_SS = "ss"
     SUB_TYPE_NORMAL = "NORMAL"
     SUB_TYPE_CLASH = "clash"
     SUB_TYPE_CLASH_PRO = "clash_pro"
 
     SUB_TYPES_SET = {
+        SUB_TYPE_SS,
         SUB_TYPE_NORMAL,
         SUB_TYPE_CLASH,
         SUB_TYPE_CLASH_PRO,
@@ -27,6 +30,8 @@ class UserSubManager:
     def get_sub_info(self):
         if self.sub_type in [self.SUB_TYPE_CLASH, self.SUB_TYPE_CLASH_PRO]:
             return self.get_clash_sub_yaml()
+        elif self.sub_type == self.SUB_TYPE_SS:
+            return self.get_ss_sub_links()
         return self.get_clash_proxy_providers()
 
     def get_clash_sub_yaml(self):
@@ -38,6 +43,23 @@ class UserSubManager:
                 "proxy_provider_url": self.user.clash_proxy_provider_endpoint,
             },
         )
+
+    def get_ss_sub_links(self):
+        sub_links = ""
+        relay_node_group = defaultdict(list)
+        for node in self.node_list:
+            if node.enable_relay:
+                for rule in node.relay_rules.filter(relay_node__enable=True):
+                    relay_node_group[rule.relay_node].append(
+                        node.get_user_node_link(self.user, rule)
+                    )
+            if node.enable_direct:
+                sub_links += node.get_user_node_link(self.user) + "\n"
+        for sub_link_list in relay_node_group.values():
+            for link in sub_link_list:
+                sub_links += link + "\n"
+        sub_links = base64.urlsafe_b64encode(sub_links.encode()).decode()
+        return sub_links
 
     def get_clash_proxy_providers(self):
         """todo support multi provider group"""
